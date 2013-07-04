b885011f8e9ed96fb0c5182152bc119cdeb3a618
Stefan heimes <stefan_heimes@hotmail.com>
2013-07-04 09:38:33 +0200

    Add a missing translation.

--- system/modules/geolocationRedirect/languages/en/tl_settings.php
+++ system/modules/geolocationRedirect/languages/en/tl_settings.php
@@ -10,6 +10,7 @@
  * @filesource
  */
 
+$GLOBALS['TL_LANG']['tl_settings']['geolocationRedirect_legend']      = 'Geolocation Redirects';
 $GLOBALS['TL_LANG']['tl_settings']['geolocationRedirectKillQueryStr'] = array('Kill Query-String','Deletes the Query-String from the url, everything behind the "?"');
 
 ?>
