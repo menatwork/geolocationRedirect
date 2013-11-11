65b9429dc7b23d577c4aa50c0d419439fd428f5c
Stefan heimes <stefan_heimes@hotmail.com>
2013-11-11 17:01:38 +0100

    Update for contao 3

---
+++ system/modules/geolocationRedirect/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package GeolocationRedirect
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'PageError404_GeolocationRedirect' => 'system/modules/geolocationRedirect/PageError404_GeolocationRedirect.php',
+));
